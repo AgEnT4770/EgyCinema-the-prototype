2e53023eb30f9c36d31c77551be479b3cb6ca47d
YoungMaster1001 <youngmaster1001@gmail.com>
2026-04-24 03:40:04 +0300

    Added a User file to handle users and assign them IDs

--- Backend/cinema-service/src/main/java/com/example/demo/security/User.java
+++ Backend/cinema-service/src/main/java/com/example/demo/security/User.java
@@ -1,5 +1,29 @@
 package com.example.demo.security;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto increments an ID for each new user
+    private Long id;
+    @Column(unique = true, nullable = false) //Username must be unique and not empty or null
+    private String username;
+    @Column(nullable = false) //Password must not be empty or null
+    private String passwordHash; //Stores the hashed password from UserAuth
+    @Column(nullable = false) //Salt must not be empty or null
+    private byte[] salt; //Stores the salt used for hashing the password
+    @Column(nullable = false) //Role must not be empty or null
+    private String role; //Stores the user role 
+    
 }
