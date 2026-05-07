773b2f1fc03f3872fd35411a77e352cc298fbf6d
ghaida <lofedmonke@gmail.com>
2026-05-07 15:12:29 +0300

    Add files via upload

---
+++ Backend/movie-service/showtime-service/src/main/java/com/egycinema/showtime_service/repositry/Movie.java
@@ -0,0 +1,41 @@
+package com.egycinema.showtime_service.repositry;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Movies")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MovieID")
+    private Long MovieID;
+
+    @Column(name = "Title")
+    private String title;
+
+    @Column(name = "ImgURL")
+    private String imgUrl;
+
+
+    @Column(name = "Duration")
+    private Integer duration;
+
+    @Column(name = "ReleaseDate")
+    private String ReleaseDate;
+
+    @Column(name = "CategoryID")
+    private Long categoryId;
+
+    @Column(name = "TrailerURL")
+    private String trailerUrl;
+
+    @Column(name = "Status")
+    private String status;
+
+}
